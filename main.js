a9431ebf9c9f380fe0403c3bd8d8aaa68bac3eba
AymanMorsy <aymanmorsy.eg@gamil.com>
2021-01-08 16:11:36 +0200

    optimize responsive header

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+// Swiper Slider init //
+
 var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     pagination: {
@@ -5,3 +8,14 @@ var swiper = new Swiper('.swiper-container', {
         clickable: true,
     },
 });
+
+// hamburger Menu Icon //
+
+var haburger = document.querySelector(".haburger-menu")
+var nav = document.querySelector("header nav")
+haburger.addEventListener("click", () => {
+    haburger.classList.toggle("open")
+    console.log(nav);
+    nav.classList.toggle("show")
+
+})
